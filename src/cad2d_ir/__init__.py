a8d2de472eec808f503695268cc23322082e8ae6
neka-nat <nekanat.stock@gmail.com>
2026-08-18 13:07:06 +0900

    fix version

--- src/cad2d_ir/__init__.py
+++ src/cad2d_ir/__init__.py
@@ -41,7 +41,7 @@ from cad2d_ir.importers import (
 )
 from cad2d_ir.schema import IRValidationError, load_schema, validate_ir
 
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 
 __all__ = [
     "__version__",
